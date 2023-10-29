3a313738a0d97656740af3f200806758591f201f
Kang <theman19970919@gmail.com>
2023-10-29 18:46:26 +0800

    Initial Commit

---
+++ SearchGithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchGithub
+//
+//  Created by 康 on 2023/10/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
